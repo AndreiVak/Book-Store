c1088c46b1227c4bc7a93ef55a71e18a24e6d121
Andrei <andrei.frdev@gmail.com>
2024-08-31 15:23:02 +0300

    project setup

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+(function () {
+    'use strict';
+
+    console.log("test");
+    console.log(2 + 4);
+    console.log("Hello my dear student");
+
+})();
---
+++ src/app.js
@@ -0,0 +1,12 @@
+"use strict"
+import "./app.css"
+
+const testFunc = () => {
+    return 5 + 5
+}
+
+testFunc()
+
+console.log("test")
+console.log(2 + 4)
+console.log("Hello my dear student")
